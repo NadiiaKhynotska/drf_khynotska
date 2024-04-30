6269aaa1a73a606a5af190cca3ac439e26e82895
NadiiaKhynotska <nadinyman@gmail.com>
2024-04-30 15:06:58 +0300

    innit commit

--- configs/settings.py
+++ configs/settings.py
@@ -31,11 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
@@ -75,8 +72,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'khynotska-2023-drf',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
